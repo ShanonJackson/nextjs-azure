8a55ee6ba8e47ab819d8b335f657bff98cdc4d75
ShanonJackson <ShanonJacksonNZ@gmail.com>
2022-08-21 14:40:02 +1200

    fix build files

--- src/pages-ui.ts
+++ src/pages-ui.ts
@@ -9,10 +9,10 @@ const mkdir = (path: string) => fs.existsSync(path) ? null : fs.mkdirSync(path);
 const getDependenciesFor = (directory: string): string[] => {
 	const dependencies = new Set<string>([]);
 	glob.sync(directory + "/**/*.json").forEach((f) => {
-		if(f.includes(`pages/api`)) return; /* don't go into pages/api folder for ui-backend */
-		const { files: deps } = JSON.parse(fs.readFileSync(f, 'utf8')) as {files: string[]};
+		if (f.includes(`pages/api`)) return; /* don't go into pages/api folder for ui-backend */
+		const {files: deps} = JSON.parse(fs.readFileSync(f, 'utf8')) as { files: string[] };
 		deps.forEach((dep) => {
-			if(dep.includes("package.json")) return;
+			if (dep.includes("package.json")) return;
 			const joined = path.join(f, '../' + dep);
 			dependencies.add(joined);
 		});
@@ -25,21 +25,35 @@ export const buildPagesUi = () => {
 	dependencies.map((depend) => {
 		mkdir(`.${path.sep}${FOLDER_NAME}`);
 		const to = `.${path.sep}${FOLDER_NAME}${path.sep}` + depend.replace(`.next${path.sep}standalone${path.sep}`, "").replace(`.next${path.sep}server`, "");
-		fs.cpSync(to, depend);
+		fs.outputFileSync(to, fs.readFileSync(depend), "utf-8");
 	});
 	glob.sync("./.next/standalone/.next/server/pages/**/*.js").map((file) => {
-		if(file.includes(`pages/api`)) return; /* not pages/api folder */
+		if (file.includes(`pages/api`)) return; /* not pages/api folder */
 		fs.outputFileSync(file.replace(`./.next/standalone/.next/server`, `./${FOLDER_NAME}`), fs.readFileSync(file), "utf-8");
 	})
 	/* copy manifest files across, these have useful data inside of them for handling routing + propagating some next.config.js options */
-	fs.cpSync('./.next/standalone/.next/server/pages-manifest.json', `./${FOLDER_NAME}/pages-manifest.json`)
-	fs.cpSync('./.next/standalone/.next/routes-manifest.json', `./${FOLDER_NAME}/routes-manifest.json`)
-	fs.cpSync('./.next/standalone/.next/build-manifest.json', `./${FOLDER_NAME}/build-manifest.json`);
-	fs.cpSync('./.next/standalone/.next/required-server-files.json', `./${FOLDER_NAME}/required-server-files.json`)
-
-
+	fs.writeFileSync(
+		`./${FOLDER_NAME}/pages-manifest.json`,
+		fs.readFileSync('./.next/standalone/.next/server/pages-manifest.json'),
+		'utf8',
+	);
+	fs.writeFileSync(
+		`./${FOLDER_NAME}/routes-manifest.json`,
+		fs.readFileSync('./.next/standalone/.next/routes-manifest.json'),
+		'utf8',
+	);
+	fs.writeFileSync(
+		`./${FOLDER_NAME}/build-manifest.json`,
+		fs.readFileSync('./.next/standalone/.next/build-manifest.json'),
+		'utf8',
+	);
+	fs.writeFileSync(
+		`./${FOLDER_NAME}/required-server-files.json`,
+		fs.readFileSync('./.next/standalone/.next/required-server-files.json'),
+		'utf8',
+	);
 	fs.cpSync(".next/standalone/node_modules/react-dom", `./${FOLDER_NAME}/node_modules/react-dom`, {recursive: true}); /* react-dom always needed */
-	if(fs.existsSync(".next/standalone/node_modules/styled-jsx")) /* styled-jsx always needed */ {
+	if (fs.existsSync(".next/standalone/node_modules/styled-jsx")) /* styled-jsx always needed */ {
 		fs.cpSync(".next/standalone/node_modules/styled-jsx", `./${FOLDER_NAME}/node_modules/styled-jsx`, {recursive: true});
 	}
 	fs.cpSync(".next/standalone/node_modules/next", `./${FOLDER_NAME}/node_modules/next`, {recursive: true}); /* always needed */
