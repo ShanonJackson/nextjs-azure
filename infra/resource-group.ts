dd6944e22ff8c23e0fb778f7c7f7e93ad46b07ba
ShanonJackson <ShanonJacksonNZ@gmail.com>
2022-08-21 14:23:04 +1200

    Frontdoor CDN configuration started, almost done, lots of cpsync changes

--- infra/resource-group.ts
+++ infra/resource-group.ts
@@ -1,3 +1,4 @@
 import * as resources from "@pulumi/azure-native/resources";
 
 /* needs to be in another file because of circular dependency */
+export const rg = new resources.ResourceGroup('nextjs-demo');
