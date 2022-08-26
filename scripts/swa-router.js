d86d38b07cc2589b52f3dde830a38d8c4f835752
ShanonJackson <ShanonJacksonNZ@gmail.com>
2022-08-27 11:43:44 +1200

    start work to optimize swa-router

--- scripts/swa-router.js
+++ scripts/swa-router.js
@@ -1,30 +1,7 @@
 const fs = require("fs");
-const walk = function(dir) {
-	let results = [];
-	const list = fs.readdirSync(dir).filter((dir) => !dir[0].startsWith('_'));
-	list.forEach(function(file) {
-		file = dir + '/' + file;
-		const stat = fs.statSync(file);
-		if (stat && stat.isDirectory()) {
-			results = results.concat(walk(file));
-		} else {
-			results.push(file.split('./out')[1]);
-		}
-	});
-	return results;
-};
 
 const config = {
 	routes: [
-		...walk('./out').map((route) => {
-			return {
-				route: route
-					.split('.')
-					.slice(0, -1)
-					.join('.'),
-				rewrite: route,
-			};
-		}),
 		{
 			route: "/*.js",
 			headers: {
